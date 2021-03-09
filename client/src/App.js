0531095b6c6e474d865c5e6ddefc9e96a86452bb
Liz <lizlarkin@highlandtechnology.com>
2021-03-08 20:41:19 -0800

    added jumbotron

--- client/src/App.js
+++ client/src/App.js
@@ -7,18 +7,23 @@ import Saved from './Pages/Saved';
 import Search from './Pages/Search';
 import Error from './Pages/Error';
 import Nav from './Components/Nav';
+import Jumbotron from './Components/Jumbotron';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        
         <Nav />
+        <Jumbotron />
+
         <Switch>
           <Route path = "/Saved" component = {Saved} />
           <Route path = "/Search" component = {Search} />
           <Route exact path = "/" component = {Home} />
           <Route path = "/" component = {Error} />
         </Switch>
+
       </Router>
     </div>
   );
---
+++ client/src/Components/Jumbotron.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import booksPicture from "./books-picture.png"
+
+const Jumbotron = () => {
+
+    const jumboStyles = {
+        marginTop: "15px",
+        height: "300px",
+
+        jumboText: {
+            color: "#0097e6",
+        }
+    }
+    return (
+        <div class = "col-md-12 jumbotron text-dark bg-light" style={jumboStyles}>
+            <h2 style={jumboStyles.jumboText}>(React) Google Books Search</h2>
+            <h4 style={jumboStyles.jumboText}>Search for and Save Books of Interest</h4>
+            <img src={booksPicture} alt="Books on a Shelf"/>
+        </div>
+    )
+}
+
+export default Jumbotron
