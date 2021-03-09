23d6cda2644b7b30813a4a6010a6a49061b6becf
Liz <lizlarkin@highlandtechnology.com>
2021-03-08 20:12:14 -0800

    added react-router-dom and basic error, home, saved, and search pages to establish navigation between pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import axios from "axios";
+import {useEffect} from "react";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from './Pages/Home';
+import Saved from './Pages/Saved';
+import Search from './Pages/Search';
+import Error from './Pages/Error';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Router>
+        <Switch>
+          <Route path = "/Saved" component = {Saved} />
+          <Route path = "/Search" component = {Search} />
+          <Route exact path = "/" component = {Home} />
+          <Route path = "/" component = {Error} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ client/src/Pages/Error.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Error = () => {
+    return (
+        <div>
+            <h1>Error</h1>
+            <p>You've reached this page in error. Please try again.</p>
+        </div>
+    )
+}
+
+export default Error
